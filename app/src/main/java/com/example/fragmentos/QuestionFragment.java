1249d3473cc0dce0b130213213aac0593eba7456
joanr <jarm1966@gmail.com>
2023-08-21 17:29:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fragmentos/QuestionFragment.java
@@ -0,0 +1,67 @@
+package com.example.fragmentos;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+
+import com.example.fragmentos.databinding.FragmentQuestionBinding;
+
+
+public class QuestionFragment extends Fragment {
+
+    private FragmentQuestionBinding mBinding;
+
+    public QuestionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+     mBinding= FragmentQuestionBinding.inflate(inflater,container,false);
+     return mBinding.getRoot();
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        mBinding.radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+
+                int index = mBinding.radiogroup.indexOfChild(mBinding.radiogroup.findViewById(i));
+                switch (index){
+
+                    case 0:
+                        mBinding.textViewRes.setText("Si lo Conozco");
+                        break;
+
+                    case 1:
+                        mBinding.textViewRes.setText("No lo conozco");
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+            }
+        });
+    }
+}
